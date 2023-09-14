1ab91f647d48053fa0356191039e15d195541102
s92509171 <s92509171@gmail.com>
2023-09-14 17:56:37 +0800

    觀測頁面

--- src/pages/Weather/index.vue
+++ src/pages/Weather/index.vue
@@ -1,79 +1,107 @@
 <template>
-  <div class="container">
-    <div class="title">
-        <h3>彭佳嶼基隆海面</h3>
-    </div>
-    <ul class="date">
-        <li class="subtitle">日期</li>
-        <li>11</li>
-        <li>22</li>
-        <li>33</li>
-    </ul>
-    <ul class="weather">
-        <li class="subtitle">天氣</li>
-        <li>11</li>
-        <li>22</li>
-        <li>33</li>
-    </ul>
-    <ul class="wind">
-        <li class="subtitle">風向</li>
-        <li>11</li>
-        <li>22</li>
-        <li>33</li>
-    </ul>
-    <ul class="waves">
-        <li class="subtitle">海浪</li>
-        <li>11</li>
-        <li>22</li>
-        <li>33</li>
-    </ul>
-    <ul class="waves-high">
-        <li class="subtitle">浪高</li>
-        <li>11</li>
-        <li>22</li>
-        <li>33</li>
-    </ul>
-    <ul class="wind-force">
-        <li class="subtitle">風力</li>
-        <li>11</li>
-        <li>22</li>
-        <li>33</li>
-    </ul>
+    <div class="container">
+        <div class="title">
+            <h3>基隆海面</h3>
+        </div>
+        <div class="weather-data">
+            <ul class="date">
+                <li class="subtitle">日期</li>
+                <li>{{ DataTime }}</li>
 
-    <div class="result">
-        <h3>今日的天氣適合:</h3>
+            </ul>
+
+            <ul class="wind">
+                <li class="subtitle">風向</li>
+                <li>{{WindDirectionDescription}}</li>
+
+            </ul>
+            <ul class="wind-force">
+                <li class="subtitle">風速</li>
+                <li>{{WindSpeed}} Kt</li>
+
+            </ul>
+            <ul class="waves">
+                <li class="subtitle">海浪</li>
+                <li>{{ WaveDirectionDescription }}</li>
+
+            </ul>
+            <ul class="waves-high">
+                <li class="subtitle">浪高</li>
+                <li>{{ WaveHeight }} m</li>
+
+            </ul>
+
+        </div>
+
+
+        <div class="result">
+            <h3>今日的天氣適合:</h3>
+        </div>
     </div>
-  </div>
 </template>
 46694A
 <script>
 import axios from 'axios'
 export default {
-name:'MyWeather',
-mounted(){
-    axios.get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-B0075-001?Authorization=CWB-C3C5CC0C-4772-40C5-8FC3-2E23656ACF12&WeatherElement=WaveHeight')
-    .then( (response) => console.log(response))
-    .catch( (error) => console.log(error))
-}
+    name: 'MyWeather',
+    data() {
+        return {
+            WaveHeight: '',
+            DataTime: '',
+            WaveDirectionDescription: '',
+            WindSpeed: '',
+            WindDirectionDescription: ''
+        }
+    },
+    async mounted() {
+        await axios.get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-B0075-001?Authorization=CWB-C3C5CC0C-4772-40C5-8FC3-2E23656ACF12&StationID=OAC004&WeatherElement=WaveHeight,WaveDirectionDescription,PrimaryAnemometer')
+            .then((response) => {
+                let WaveHeight = response.data.Records.SeaSurfaceObs.Location[0].StationObsTimes.StationObsTime[20].WeatherElements.WaveHeight
+
+                let DataTime = response.data.Records.SeaSurfaceObs.Location[0].StationObsTimes.StationObsTime[20].DateTime
+
+                let WaveDirectionDescription = response.data.Records.SeaSurfaceObs.Location[0].StationObsTimes.StationObsTime[20].WeatherElements.WaveDirectionDescription
+
+                let WindSpeed = response.data.Records.SeaSurfaceObs.Location[0].StationObsTimes.StationObsTime[20].WeatherElements.PrimaryAnemometer.WindSpeed
+
+                let WindDirectionDescription = response.data.Records.SeaSurfaceObs.Location[0].StationObsTimes.StationObsTime[20].WeatherElements.PrimaryAnemometer.WindDirectionDescription
+
+                this.WaveHeight = WaveHeight
+                this.DataTime = DataTime.split('T')[0]
+                this.WaveDirectionDescription = WaveDirectionDescription
+                this.WindSpeed = WindSpeed
+                this.WindDirectionDescription = WindDirectionDescription
+
+                console.log(response)
+                console.log(DataTime)
+            })
+
+
+            .catch((error) => console.log(error))
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-.container{
+.container {
     width: 1200px;
     margin: 0 auto;
     text-align: center;
-    ul{
+    margin-top: 40px;
+    ul {
         display: flex;
         justify-content: center;
-        li{
+
+        li {
             border: 1px solid;
             padding: 10px 20px;
             margin-bottom: -1px;
             margin-left: -1px;
+            width: 200px;
         }
     }
+    .weather-data{
+        margin: 20px 0;
+    }
 }
-
-
 </style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -51,7 +51,7 @@ export default[
         },
         {
             path:'/',
-            redirect:'/Home'
+            redirect:'/About'
         }
     
 ]
